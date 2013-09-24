645a142e43d5c1fdd5451de743f3354da93e59e4
Adrian Goins <agoins@arces.net>
2013-09-23 21:16:14 -0300

    set unicorn to log stderr/stdout so supervisor can capture it

--- docker_files/unicorn.rb
+++ docker_files/unicorn.rb
@@ -37,8 +37,8 @@ pid "/home/git/gitlab/tmp/pids/unicorn.pid"
 # By default, the Unicorn logger will write to stderr.
 # Additionally, some applications/frameworks log to stderr or stdout,
 # so prevent them from going to /dev/null when daemonized here:
-stderr_path "/home/git/gitlab/log/unicorn.stderr.log"
-stdout_path "/home/git/gitlab/log/unicorn.stdout.log"
+#stderr_path "/home/git/gitlab/log/unicorn.stderr.log"
+#stdout_path "/home/git/gitlab/log/unicorn.stdout.log"
 
 # combine Ruby 2.0.0dev or REE with "preload_app true" for memory savings
 # http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
